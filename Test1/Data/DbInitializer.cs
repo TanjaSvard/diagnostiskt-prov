6c187b076f6a089f29bc916e3f8cda762dc6cf00
Tanja Svärd <tanja_jazz@hotmail.com>
2017-09-07 10:35:24 +0200

    Added new categories to context

--- Test1/Data/DbInitializer.cs
+++ Test1/Data/DbInitializer.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Test1.Models;
 
 namespace Test1.Data
 {
     public class DbInitializer
     {
+        public static void Initialize(ApplicationDbContext context)
+        {
 
-    }
+
+            if (!context.Products.Any())
+            {
+                var tv = new ProductCategory { Name = "TV"};
+                var dvd = new ProductCategory { Name = "DVD"};
+                var vhs = new ProductCategory { Name = "VHS"};
+
+                context.ProductCategories.AddRange(tv, dvd, vhs);
+            }
+            }
+        }
 }
